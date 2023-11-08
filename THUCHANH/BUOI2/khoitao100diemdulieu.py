a77f38ae1b4a12181a5d9dd90d617668ea0adf17
UIT-21521226 <116283788+nmnhatUIT03@users.noreply.github.com>
2023-11-08 13:09:51 +0700

    ok

---
+++ THUCHANH/BUOI2/khoitao100diemdulieu.py
@@ -0,0 +1,10 @@
+# khởi tạo 100 điểm dữ liệu gồm x và y
+import numpy as np
+import matplotlib.pyplot as plt
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+plt.plot(X, y, "b.")
+X_b = np.c_[np.ones((100, 1)), X]
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+
